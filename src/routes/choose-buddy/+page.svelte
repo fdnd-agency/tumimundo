112301562f8d8a39cf91d3e7c12c405030f939ee
lemuelmgsn <magsinolemuel68@gmail.com>
2025-02-27 12:37:47 +0100

    feat: made static html and styling

---
+++ src/routes/choose-buddy/+page.svelte
@@ -0,0 +1,33 @@
+
+
+<main>
+    <section>
+        <h1>Choose a buddy</h1>
+        <p>During the learning journey your child will have a buddy that learns with them, gives reminders and shows the statistics! Here you can pick a buddy.</p>
+
+        <p>Drag left or right, or use the buttons on the side to see all the buddies!</p> 
+    </section>
+    
+</main>
+
+<style>
+
+main{
+    background: var(--bg-image-blue);
+    color: white;
+    height: 100vh;
+    width: 100vw;
+}    
+section{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+h1{
+    margin: 2em 0 2em 0;
+}
+p{
+    max-width: 30em;
+    margin-bottom: 1em;
+}
+</style>
