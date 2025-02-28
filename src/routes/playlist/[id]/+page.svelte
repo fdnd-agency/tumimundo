8ab145a476fc14a0863c37a09708022d82fae0b8
lemuelmgsn <magsinolemuel68@gmail.com>
2025-02-28 09:47:11 +0100

    perf: improved performance and accessibility

--- src/routes/playlist/[id]/+page.svelte
+++ src/routes/playlist/[id]/+page.svelte
@@ -8,21 +8,21 @@
 
 <main>
   <nav>
-    <a href="/lessons">
+    <a href="/lessons" aria-label="go back">
       <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M26.3398 8.55508C27.0797 9.29502 27.0797 10.4947 26.3398 11.2346L17.5743 20.0001L26.3398 28.7656C27.0797 29.5055 27.0797 30.7052 26.3398 31.4452C25.5998 32.1851 24.4002 32.1851 23.6602 31.4452L13.555 21.3399C12.815 20.6 12.815 19.4003 13.555 18.6603L23.6602 8.55508C24.4002 7.81514 25.5998 7.81514 26.3398 8.55508Z" fill="black"/>
       </svg>
     </a>
 
     <div class="dots-container">
-      <button class="dots" aria-expanded="false">
+      <button class="dots" aria-expanded="false" aria-label="More options">
         <div class="dot"></div>
         <div class="dot"></div>
         <div class="dot"></div>
       </button>
       <ul class="dropdown">
         <li>
-          <a href="#" class="dropdown-items">
+          <a href="#share" class="dropdown-items" aria-label="share playlist">
             <div class="text">Share</div>
             <div class="icon">
               <!-- convert SVG into svg component after merging -->
@@ -34,7 +34,7 @@
           </a>
         </li>
         <li>
-          <a href="#" class="dropdown-items">
+          <a href="#edit-playlist" class="dropdown-items" aria-label="edit playlist">
             <div class="text">Edit playlist</div>
             <div class="icon">
               <!-- convert SVG into svg component after merging -->
@@ -46,7 +46,7 @@
           </a>
         </li>
         <li>
-          <a href="#" class="dropdown-items">
+          <a href="#delete" class="dropdown-items" aria-label="delete playlist">
             <div class="text-delete">Delete</div>
             <div class="icon">
               <!-- convert SVG into svg component after merging -->
@@ -79,16 +79,16 @@
     <p>{playlist.description}</p>
 
     <div class="meta-info">
-      <img src="/icons/profile-icon.svg" alt="profile picture">
+      <img src="/icons/profile-icon.svg" alt="profile picture" height="30">
       <p>Made by <strong>User { playlist.creator }</strong></p>
-      <img src="/icons/clock.svg" alt="time">
+      <img src="/icons/clock.svg" alt="time" height="20">
       <p>2u 11m</p>
     </div>
 
     <div class="meta-play">
-      <a><img src="/icons/download.svg" alt="download"></a>
-      <a class="heart-svg"><img src="/icons/heart.svg" alt="like"></a>
-      <a><img src="/icons/play.svg" alt="play"></a>
+      <a><img src="/icons/download.svg" alt="download" height="27"></a>
+      <a class="heart-svg"><img src="/icons/heart.svg" alt="like" height="35"></a>
+      <a><img src="/icons/play.svg" alt="play" height="60"></a>
     </div>
   </section>
 
@@ -96,7 +96,9 @@
     <section class="stories-section">
       <ul>
         {#each playlist.stories as story}
+        <li>
           <Story {story} />
+        </li>
         {/each}
       </ul>
     </section>
