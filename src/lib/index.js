e037e1b90105943c033e7e80a3252899da147e1d
lemuelmgsn <magsinolemuel68@gmail.com>
2025-02-28 09:53:13 +0100

    refactor: refactored dropdown into component and removed unused code

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,6 +7,7 @@ export { default as Search } from '$lib/components/forms/search.svelte'
 export { default as Playlist } from '$lib/components/layout/playlist.svelte'
 export { default as Popup } from '$lib/components/layout/popup.svelte'
 export { default as Story } from '$lib/components/layout/story.svelte'
+export { default as Dropdown } from '$lib/components/core/dropdown.svelte'
 
 // Helpers
 export { default as fetchJson } from '$lib/components/helpers/fetch-json.js'
