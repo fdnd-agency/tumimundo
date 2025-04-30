221557bce6c2e32e1c7378279aae4bbd3335b1c8
lemuelmgsn <magsinolemuel68@gmail.com>
2025-04-30 15:56:24 +0200

    fix: fixed HTML validator improvement points
    references #277

--- src/lib/components/core/dropdown.svelte
+++ src/lib/components/core/dropdown.svelte
@@ -1,8 +1,8 @@
 <div class="dots-container">
   <button class="dots" aria-expanded="false" aria-label="More options">
-    <div class="dot"></div>
-    <div class="dot"></div>
-    <div class="dot"></div>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
   </button>
   <ul class="dropdown">
     <li>
