868755a67f9e1ed1272055b39379eac8f57ab266
lemuelmgsn <magsinolemuel68@gmail.com>
2025-06-02 11:31:02 +0200

    fix: popup name

--- src/lib/index.js
+++ src/lib/index.js
@@ -11,7 +11,7 @@ export { default as Search } from '$lib/components/forms/Search.svelte'
 export { default as AddStory } from '$lib/components/forms/AddStory.svelte';
 
 export { default as Playlist } from '$lib/components/layout/Playlist.svelte'
-export { default as Popup } from '$lib/components/layout/popup.svelte'
+export { default as Popup } from '$lib/components/layout/Popup.svelte'
 export { default as Story } from '$lib/components/layout/story.svelte'
 
 
